a6dbd7f0b0b0b3d3afc6ac387ad13cded5a42ade
codeless <mh@unixassembler.at>
2012-04-23 16:53:24 +0200

    Added the resy theme

---
+++ resy/lang_de.inc.php
@@ -0,0 +1,8 @@
+<?php
+@define("RESY_COLORSET", "Farbschema");
+@define("RESY_COLORSET_DEFAULT", "Standard");
+@define("RESY_COLORSET_SERENDIPITY", "Serendipity");
+@define("RESY_COLORSET_LIME_AND_MELON_SALAD", "Limetten-Melonen-Salat");
+@define("RESY_SIDEBAR_POSITION", "Position der Seitenleiste");
+@define("RESY_LEFT", "links");
+@define("RESY_RIGHT", "rechts");
---
+++ resy/lang_en.inc.php
@@ -0,0 +1,8 @@
+<?php
+@define("RESY_COLORSET", "Colorset");
+@define("RESY_COLORSET_DEFAULT", "Default");
+@define("RESY_COLORSET_SERENDIPITY", "Serendipity");
+@define("RESY_COLORSET_LIME_AND_MELON_SALAD", "Lime and melon salad");
+@define("RESY_SIDEBAR_POSITION", "Sidebar position");
+@define("RESY_LEFT", "left");
+@define("RESY_RIGHT", "right");
