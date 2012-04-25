215db90d029b8840a50a10aa6e3e50aa09a25daf
codeless <mh@unixassembler.at>
2012-04-25 11:35:56 +0200

    Updated the RESY theme

--- resy/lang_de.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-@define("RESY_COLORSET", "Farbschema");
-@define("RESY_COLORSET_DEFAULT", "Standard");
-@define("RESY_COLORSET_SERENDIPITY", "Serendipity");
-@define("RESY_COLORSET_LIME_AND_MELON_SALAD", "Limetten-Melonen-Salat");
-@define("RESY_SIDEBAR_POSITION", "Position der Seitenleiste");
-@define("RESY_LEFT", "links");
-@define("RESY_RIGHT", "rechts");
