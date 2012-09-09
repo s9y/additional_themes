bb8506b4276255ad9acc56dac3bab544b7d09c89
Vladimir Ajgl <vlada@ajgl.cz>
2012-09-09 20:26:21 +0200

    czech translation for resy theme

---
+++ resy/lang_cs.inc.php
@@ -0,0 +1,14 @@
+<?php # lang_cs.inc.php 1.0 2012-05-13 14:11:16 VladaAjgl $
+
+/**
+ *  @version 1.0
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @translated 2012/05/13
+ */
+@define('RESY_COLORSET', 'Barevné schéma');
+@define('RESY_COLORSET_DEFAULT', 'Výchozí');
+@define('RESY_COLORSET_SERENDIPITY', 'Serendipity');
+@define('RESY_COLORSET_LIME_AND_MELON_SALAD', 'Limetkovo-melounový salát');
+@define('RESY_SIDEBAR_POSITION', 'Poloha postranního sloupce');
+@define('RESY_LEFT', 'vlevo');
+@define('RESY_RIGHT', 'vpravo');
