4c85d409e7d3f2c630b28e0b59870921f4f8eecb
Don Chambers <don.chambers@optional-necessity.com>
2015-03-10 17:42:37 -0500

    lets try that again

---
+++ 1024px/config.inc.php
@@ -0,0 +1,72 @@
+<?php # 
+
+$probelang = dirname(__FILE__) . '/lang_' . $serendipity['lang'] . '.inc.php';
+if (file_exists($probelang)) {
+    include $probelang;
+} else {
+    include dirname(__FILE__) . '/lang_en.inc.php';
+}
+$template_config = array(
+    array(
+        'var'           => 'colorset',
+        'description'   => 'Select colorset option',
+        'type'          => 'select',
+        'default'       => 'blue',
+        'select_values' => array('default' => 'Blue', 'red' => 'Red')
+    ),
+    array(
+        'var'           => 'contentposition',
+        'title'         => 'Sidebar Position',
+        'description'   => 'Sidebar left or right',
+        'type'          => 'radio',
+	  'radio'         => array('value' => array('true', 'false'),
+					'desc' => array('Left', 'Right')),
+        'default'       => 'true',
+    ),
+    array(
+        'var'           => 'sitename',
+        'title'         => 'Your Business Name',
+        'description'   => 'Enter the year and business name',
+        'type'          => 'string',
+        'default'       => '2006 Your Business Name',
+    ),
+    array(
+        'var'           => 'amount',
+        'name'          => 'Number of navlinks',
+        'description'   => 'Enter the number of navlinks you want to use in the navbar.',
+        'type'          => 'string',
+        'default'       => '3',
+    ),
+);
+
+if (version_compare($serendipity['version'],"1.1.beta3") >= 0) {
+$navlinks = array();
+
+for ($i = 0; $i < $vars['amount']; $i++) {
+    $navlinks[] = array(
+        'title' => $vars['navlink' . $i . 'text'],
+        'href'  => $vars['navlink' . $i . 'url']
+    );
+    $template_config[] = array(
+        'var'           => 'navlink' . $i . 'text',
+        'name'          => NAV_LINK_TEXT . ' #' . $i,
+        'description'   => NAV_LINK_DESC . ' #' .$i,
+        'type'          => 'string',
+        'default'       => NAV_DEFAULT . ' #' . $i,
+	);
+    $template_config[] = array(
+        'var'           => 'navlink' . $i . 'url',
+        'name'          => NAV_LINK_URL . ' #' . $i,
+        'description'   => NAV_LINK_URL_DESC . ' #' . $i,
+        'type'          => 'string',
+        'default'       => '#',
+    );
+}
+
+$serendipity['smarty']->assign_by_ref('navlinks', $navlinks);
+}
+$template_global_config = array('navigation' => true);
+$template_loaded_config = serendipity_loadThemeOptions($template_config, $serendipity['smarty_vars']['template_option'], true);
+serendipity_loadGlobalThemeOptions($template_config, $template_loaded_config, $template_global_config);
+$template_config_groups = NULL;
+?>
