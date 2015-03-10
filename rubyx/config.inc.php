e7db624c879f255456a5b7f6aaff1e209ae440a3
Don Chambers <don.chambers@optional-necessity.com>
2015-03-10 11:12:04 -0500

    Create missing language file

--- rubyx/config.inc.php
+++ rubyx/config.inc.php
@@ -1,79 +1,65 @@
 <?php # 
 
-$probelang = dirname(__FILE__) . '/lang_' . $serendipity['lang'] . '.inc.php';
+$probelang = dirname(__FILE__) . '/' . $serendipity['charset'] . 'lang_' . $serendipity['lang'] . '.inc.php';
 if (file_exists($probelang)) {
     include $probelang;
-} else {
-    include dirname(__FILE__) . '/lang_en.inc.php';
 }
+include dirname(__FILE__) . '/lang_en.inc.php';
+
 $template_config = array(
     array(
         'var'           => 'navlink1text',
-        'title'         => 'Navlink #1 Text',
-        'description'   => 'Enter the first navigation text',
+        'title'         => NAVLINK1_TEXT_TITLE,
+        'description'   => NAVLINK1_TEXT_DESC,
         'type'          => 'string',
-        'default'       => 'About',
+        'default'       => NAVLINK1_TEXT_DEFAULT,
     ),
     array(
         'var'           => 'navlink1url',
-        'title'         => 'Navlink #1 URL',
-        'description'   => 'Enter the first navigation URL eg \'http://www.somesite.url\'',
+        'title'         => NAVLINK1_URL_TITLE,
+        'description'   => NAVLINK1_URL_DESC,
         'type'          => 'string',
         'default'       => '#',
     ),
     array(
         'var'           => 'navlink2text',
-        'title'         => 'Navlink #2 Text',
-        'description'   => 'Enter the second navigation text',
+        'title'         => NAVLINK2_TEXT_TITLE,
+        'description'   => NAVLINK2_TEXT_DESC,
         'type'          => 'string',
-        'default'       => 'Photos',
+        'default'       => NAVLINK2_TEXT_DEFAULT,
     ),
     array(
         'var'           => 'navlink2url',
-        'title'         => 'Navlink #2 URL',
-        'description'   => 'Enter the second navigation URL eg \'http://www.somesite.url\'',
+        'title'         => NAVLINK2_URL_TITLE,
+        'description'   => NAVLINK2_URL_DESC,
         'type'          => 'string',
         'default'       => '#',
     ),
     array(
         'var'           => 'navlink3text',
-        'title'         => 'Navlink #3 Text',
-        'description'   => 'Enter the third navigation text',
+        'title'         => NAVLINK3_TEXT_TITLE,
+        'description'   => NAVLINK3_TEXT_DESC,
         'type'          => 'string',
-        'default'       => 'Projects',
+        'default'       => NAVLINK3_TEXT_DEFAULT,
     ),
     array(
         'var'           => 'navlink3url',
-        'title'         => 'Navlink #3 URL',
-        'description'   => 'Enter the third navigation URL eg \'http://www.somesite.url\'',
+        'title'         => NAVLINK3_URL_TITLE,
+        'description'   => NAVLINK3_URL_DESC,
         'type'          => 'string',
         'default'       => '#',
     ),
     array(
         'var'           => 'navlink4text',
-        'title'         => 'Navlink #4 Text',
-        'description'   => 'Enter the fourth navigation text',
+        'title'         => NAVLINK4_TEXT_TITLE,
+        'description'   => NAVLINK4_TEXT_DESC,
         'type'          => 'string',
-        'default'       => 'Projects',
+        'default'       => NAVLINK4_TEXT_DEFAULT,
     ),
     array(
         'var'           => 'navlink4url',
-        'title'         => 'Navlink #4 URL',
-        'description'   => 'Enter the fourth  navigation URL eg \'http://www.somesite.url\'',
-        'type'          => 'string',
-        'default'       => '#',
-    ),
-    array(
-        'var'           => 'navlink5text',
-        'title'         => 'Navlink #5 Text',
-        'description'   => 'Enter the fifth navigation text',
-        'type'          => 'string',
-        'default'       => 'Projects',
-    ),
-    array(
-        'var'           => 'navlink5url',
-        'title'         => 'Navlink #5 URL',
-        'description'   => 'Enter the fifth navigation URL eg \'http://www.somesite.url\'',
+        'title'         => NAVLINK4_URL_TITLE,
+        'description'   => NAVLINK4_URL_DESC,
         'type'          => 'string',
         'default'       => '#',
     ),
