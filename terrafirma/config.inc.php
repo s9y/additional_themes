e4ee15eda3d22287b2c5b3f13bd518233e8a1daa
Matthias Mees <mm@yellowled.de>
2015-12-21 12:12:02 +0100

    Remove extraneous whitespace, fixed indentation

--- terrafirma/config.inc.php
+++ terrafirma/config.inc.php
@@ -1,33 +1,22 @@
 <?php
-
 // Be nice to the frontend users. They don't need the additional constants
-
 // and file lookups. Only load them when in Admin mode.
-
 if ($serendipity['GET']['adminModule'] == 'templates' || $serendipity['POST']['adminModule'] != 'templates') {
-
     // Probe for a language include with constants. Still include defines
-
     // later on, if some constants were missing
-
     $probelang = dirname(__FILE__) . '/' . $serendipity['charset'] . 'lang_' . $serendipity['lang'] . '.inc.php';
 
     if (file_exists($probelang)) {
-
         include $probelang;
-
     }
 
-
-
     include dirname(__FILE__) . '/lang_en.inc.php';
-
 }
 
 $serendipity['smarty']->assign(array('currpage'=> "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']));
 
 $template_config = array(
-array(
+    array(
         'var'           => 'abouttitle',
         'title'         => 'abouttitle',
 		'name'          => ABOUT_TITLE,
@@ -35,7 +24,7 @@ array(
         'type'          => 'string',
         'default'       => 'About',
     ),
-array(
+    array(
         'var'           => 'about',
         'title'         => 'about_text',
 		'name'          => ABOUT_TEXT,
@@ -51,84 +40,53 @@ array(
                                  'desc'  => array(AT_POS, AT_NEG)),
         'default'       => 'false'
     ),
-   		array(
+    array(
         'var'           => 'aboutpageurl',
         'title'         => 'About page URL',
         'description'   => ABOUT_URL,
         'type'          => 'string',
         'default'       => '#',
     ),
-		array(
+    array(
         'var'           => 'amount',
         'name'          => NAVNUM,
         'description'   => NAVNUMDESC,
         'type'          => 'string',
         'default'       => '4',
     ),
-
 );
 
 $template_config_groups = NULL;
 
 if (version_compare($serendipity['version'],"1.1.beta3") >= 0) {
+    $vars = serendipity_loadThemeOptions($template_config);
 
+    $navlinks = array();
 
+    for ($i = 0; $i < $vars['amount']; $i++) {
+        $navlinks[] = array(
+            'title' => $vars['navlink' . $i . 'text'],
+            'href'  => $vars['navlink' . $i . 'url']
+        );
 
-$vars = serendipity_loadThemeOptions($template_config);
+        $template_config[] = array(
+            'var'           => 'navlink' . $i . 'text',
+            'name'          => NAV_LINK_TEXT . ' #' . $i,
+            'description'   => NAV_LINK_DESC . ' #' .$i,
+            'type'          => 'string',
+            'default'       => constant('NAV_DEFAULT_' . $i),
+        );
 
+        $template_config[] = array(
+            'var'           => 'navlink' . $i . 'url',
+            'name'          => NAV_LINK_URL . ' #' . $i,
+            'description'   => NAV_LINK_URL_DESC . ' #' . $i,
+            'type'          => 'string',
+            'default'       => '#',
+        );
+    }
 
-
-$navlinks = array();
-
-
-
-for ($i = 0; $i < $vars['amount']; $i++) {
-
-    $navlinks[] = array(
-
-        'title' => $vars['navlink' . $i . 'text'],
-
-        'href'  => $vars['navlink' . $i . 'url']
-
-    );
-
-    $template_config[] = array(
-
-        'var'           => 'navlink' . $i . 'text',
-
-        'name'          => NAV_LINK_TEXT . ' #' . $i,
-
-        'description'   => NAV_LINK_DESC . ' #' .$i,
-
-        'type'          => 'string',
-
-        'default'       => constant('NAV_DEFAULT_' . $i),
-
-	);
-
-    $template_config[] = array(
-
-        'var'           => 'navlink' . $i . 'url',
-
-        'name'          => NAV_LINK_URL . ' #' . $i,
-
-        'description'   => NAV_LINK_URL_DESC . ' #' . $i,
-
-        'type'          => 'string',
-
-        'default'       => '#',
-
-    );
-
+    $serendipity['smarty']->assign_by_ref('navlinks', $navlinks);
+    $serendipity['smarty']->assign_by_ref('subnavlinks', $subnavlinks);
 }
-
-
-
-
-
-$serendipity['smarty']->assign_by_ref('navlinks', $navlinks);
-$serendipity['smarty']->assign_by_ref('subnavlinks', $subnavlinks);
-
-}
-
 ?>
