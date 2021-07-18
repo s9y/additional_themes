a0597f144d5b8c8e88b6cda8b1bd605faf901e73
onli <onli@paskuda.biz>
2021-07-18 23:13:09 +0200

    idea: PHP 8 compat fix by adding 2k11 language files

---
+++ idea/lang_cz.inc.php
@@ -0,0 +1,47 @@
+<?php # lang_cz.inc.php 1.2 2013-03-31 13:05:31 VladaAjgl $
+
+/**
+ *  @version 1.2
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @translated 2012/01/08
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @revisionDate 2013/03/03
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @revisionDate 2013/03/31
+ */
+@define('TWOK11_HEADER_IMG','Pou¾ít banner - obrázek v záhlaví stránky? (1120 pixelù ¹iroký; ponechte prázdné políèko a nebude se pou¾ívat ¾ádný banner)');
+@define('TWOK11_USE_CORENAV', 'Pou¾ít globální navigaci?');
+@define('TWOK11_WEBFONTS', 'Pou¾ít webfont hostovaný u Googlu?');
+@define('TWOK11_NOWEBFONT', 'Nevkládat ¾ádný webfont');
+@define('TWOK11_USERSTYLES', 'Vkládat user.css? (Je tøeba vytvoøit tento soubor v adresáøi ¹ablony. Generátor souborù na: http://yellowled.github.com)');
+// Lang constants
+
+@define('TWOK11_NAV_TITLE', 'Navigace');
+@define('TWOK11_PAG_TITLE', 'Stránkování');
+@define('TWOK11_PLINK_TEXT', 'Odkaz');
+@define('TWOK11_PLINK_TITLE', 'Stálý odkaz na tento komentáø');
+@define('TWOK11_PLACE_SEARCH', 'Vyhledat výraz(y)');
+@define('TWOK11_PLACE_NAME', 'napø. Honza Králík');
+@define('TWOK11_PLACE_MAIL', 'napø. mail@priklad.cz');
+@define('TWOK11_PLACE_URL', 'napø. http://priklad.cz');
+@define('TWOK11_PLACE_MESSAGE', 'Co máte na jazyku?');
+@define('TWOK11_SEND_MAIL', 'Poslat');
+@define('TWOK11_TWEET_THIS', 'Sdílet pøíspìvek na Twitteru');
+@define('TWOK11_DENT_THIS', 'Sdílet pøíspìvek na Identice');
+@define('TWOK11_SHORT_URL', 'Krátké URL');
+@define('TWOK11_SHORT_URL_HINT', 'Tento odkaz není my¹len ke klikání. Obsahuje krátkou URL adresu k tomuto pøíspìvku. Mù¾ete ji pou¾ít k odkazování na tento pøíspìvek. Zkopírujte kliknutím pravým tlaèítkem a následnì kliknìte na Kopírovat odkaz v Internet Exporeru nebo Kopírovat umístìní odkazu v Mozille.');
+
+// Next lines were translated on 2013/03/03
+
+@define('TWOK11_REFCOMMENTS', 'Pøidat ke komntáøùm odkaz na pøíbuzné komentáøe (pokud jsou odpovìdí na jiný komentáø)?');
+@define('TWOK11_SIDEBAR', 'Postranní sloupec');
+@define('TWOK11_EMPTYTRACKBACK', 'Obsah této Odezvy bohu¾el nemù¾e být zobrazen.');
+@define('TWOK11_TRACKBACKPREVIEW', 'Ukázat náhled');
+@define('TWOK11_REPLYORIGIN', 'Pùvodní');
+@define('TWOK11_FORMOPTIONS', 'Mo¾nosti formuláøe');
+
+// Next lines were translated on 2013/03/31
+@define('TWOK11_IMGSTYLE', 'Vzhled obrázkù v pøíspìvcích');
+@define('TWOK11_IMGSTYLE_NONE', 'Bez rámeèku');
+@define('TWOK11_IMGSTYLE_BORDER', 'Jednoduchý rámeèek');
+@define('TWOK11_IMGSTYLE_SHADOW', 'Stínovaný rámeèek');
